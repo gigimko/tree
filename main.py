1d8221952f9ad4efc55d48ca5dbdb2d4db723224
gigimko <nowyay123456@gmail.com>
2025-11-22 08:38:16 -0500

    cfcr

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.children = []
-    def add(self, data):
-        self.children.append(data)
-    def display(self):
-        print(self.data)
-        if self.children:
-            for child in self.children:
-                child.display()
-        
-            
-            
+    
+    def add(self, child_node):
+        self.children.append(child_node)
+    
+    def display(self, level):
+      
+        indent = "  " * level
+        print(indent + '|_' + str(self.data))
+        for child in self.children:
+            child.display(level + 1)
+
+
 
 electronics = Node('electronics')
+
 tvsNode = Node("TVs")
 tvsNode.add(Node("sony"))
-electronics.add(tvsNode)
 
-electronics.display()
+electronics.add(tvsNode)
+electronics.add(Node('Test'))
+electronics.display(0)
